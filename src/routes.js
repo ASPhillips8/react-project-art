02b87147eddfe09b02152b648621da0c599baf23
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-02 16:11:19 -0400

    Create Error Page

--- src/routes.js
+++ src/routes.js
@@ -3,27 +3,33 @@ import ArtExhibit from "./pages/ArtExhibit"
 import Artists from "./pages/Artists"
 import Genre from "./pages/Genre"
 import Artwork from "./pages/Artwork"
+import ErrorPage from "./pages/ErrorPage"
 
 const routes = [
   {
     path: "/",
     element: <Home />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/artworks",
     element: <ArtExhibit />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/artists",
     element: <Artists />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/genres",
     element: <Genre />,
+    errorElement: <ErrorPage />,
   },
   {
     path: "/artwork/:id",
     element: <Artwork />,
+    errorElement: <ErrorPage />,
   },
 ]
 
