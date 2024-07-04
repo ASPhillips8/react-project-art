35d750b95cb106c6f01b24e707eb7fe377e20ed3
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-07-04 16:34:09 -0400

    Create artist service file

---
+++ src/services/artistService.js
@@ -0,0 +1,21 @@
+import { createArtist, updateArtists } from "./fetcher"
+
+const getOrCreateArtist = async (artistNameInput, artists) => {
+  let artistEntry = artists.find((artist) => artist.name === artistNameInput)
+  if (!artistEntry) {
+    artistEntry = {
+      id: (artist.length + 1).toString(),
+      name: artistNameInput,
+      artworks: [],
+    }
+    await createArtist(artistEntry)
+  }
+  return artistEntry
+}
+
+const updateArtistsWithArtwork = async (artistEntry, artworkId) => {
+  artistEntry.artworks.push(artworkId)
+  await updateArtists(artistEntry.id, artistEntry)
+}
+
+export { getOrCreateArtist, updateArtistsWithArtwork }
