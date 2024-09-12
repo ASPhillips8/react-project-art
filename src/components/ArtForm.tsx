1a897b16c4a017902a010f757f8072d26ec91da3
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-09-12 09:38:07 -0400

    Convert artform to tsx

--- src/components/ArtForm.tsx
+++ src/components/ArtForm.tsx
@@ -1,6 +1,6 @@
 import React from "react"
 
-function ArtForm({ formData, onFormInput, onFormSubmit }) {
+const ArtForm: React.FC = ({ formData, onFormInput, onFormSubmit }) => {
   return (
     <form onSubmit={onFormSubmit}>
       <input
